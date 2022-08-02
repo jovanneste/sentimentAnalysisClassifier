2d7ad3ac43336d8229cc82b5951f5703f4bf0401
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-08-02 18:54:22 +0100

    GUI added using tkinter

--- predict.py
+++ predict.py
@@ -3,38 +3,64 @@ import spacy
 import numpy as np
 import pandas as pd
 
+from tkinter import *
+
 nlp = spacy.load('en_core_web_sm', disable=["ner"])
 nlp.remove_pipe('tagger')
 nlp.remove_pipe('parser')
 
 
 def text_pipeline_spacy(text):
-	tokens = []
-	doc = nlp(text)
-	for t in doc:
-		if not t.is_stop and not t.is_punct and not t.is_space:
-			tokens.append(t.lemma_.lower())
-	return tokens
-
-nb_model = pickle.load(open('nb_model.sav', 'rb'))
-logr_model = pickle.load(open('logr_model.sav', 'rb'))
-one_hot_vectorizer = pickle.load(open('one_hot_vectorizer.sav', 'rb'))
+    tokens = []
+    doc = nlp(text)
+    for t in doc:
+        if not t.is_stop and not t.is_punct and not t.is_space:
+            tokens.append(t.lemma_.lower())
+    return tokens
 
 
-sentence = input("Enter sentence: ")
-sentence_features = one_hot_vectorizer.transform([sentence])
+def predict(sentence):
+    nb_model = pickle.load(open('nb_model.sav', 'rb'))
+    logr_model = pickle.load(open('logr_model.sav', 'rb'))
+    one_hot_vectorizer = pickle.load(open('one_hot_vectorizer.sav', 'rb'))
 
-predictionLR = int(logr_model.predict(sentence_features))
-predictionNB = int(nb_model.predict(sentence_features))
+    sentence_features = one_hot_vectorizer.transform([sentence])
 
-#print("LR prediction: " + str(predictionLR))
-#print("NB prediction: " + str(predictionNB))
+    predictionLR = int(logr_model.predict(sentence_features))
+    predictionNB = int(nb_model.predict(sentence_features))
 
-prediction = (predictionLR+predictionNB)//2
+    prediction = (predictionLR+predictionNB)//2
 
-if prediction==0:
-    print("Negative sentiment")
-elif prediction==4:
-    print("Positive sentiment")
-else:
-    print("Neutral sentiment")
+    if prediction==0:
+        return "Negative sentiment"
+    elif prediction==4:
+        return "Positive sentiment"
+    else:
+        return "Neutral sentiment"
+
+
+window = Tk()
+
+window.title("Sentiment Analysis Predictor")
+window.geometry('450x300')
+
+lbl1 = Label(window, text="Enter sentence below", font=("Arial", 15))
+lbl1.grid(column=100, row=0)
+
+lbl2 = Label(window, text="", font=("Arial", 20))
+lbl2.grid(column=100, row=350)
+
+txt = Entry(window,width=50)
+txt.grid(column=100, row=50)
+
+def clicked():
+    sentence = txt.get()
+    predicted_sentiment = predict(sentence)
+    print(predicted_sentiment)
+    lbl2.configure(text=predicted_sentiment)
+
+btn1 = Button(window, text="Okay", command=clicked)
+btn1.grid(column=100, row=150)
+
+
+window.mainloop()
